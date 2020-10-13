bbb5ccbaf28649d3233933408fdcec76ed3f021e
anton <akumaigorodski@gmail.com>
2020-10-13 11:37:48 +0300

    Add HC messages and codecs

---
+++ src/main/scala/fr/acinq/hc/app/DataBags.scala
@@ -0,0 +1,90 @@
+package fr.acinq.hc.app
+
+import fr.acinq.bitcoin.{ByteVector32, ByteVector64, Crypto, LexicographicalOrdering, Protocol, Satoshi}
+import fr.acinq.eclair.wire.{Color, LightningMessageCodecs, UpdateAddHtlc}
+import fr.acinq.bitcoin.Crypto.{PrivateKey, PublicKey}
+import fr.acinq.eclair.{MilliSatoshi, UInt64}
+import scodec.bits.ByteVector
+import java.nio.ByteOrder
+
+
+trait HostedChannelMessage
+
+case class InvokeHostedChannel(chainHash: ByteVector32,
+                               refundScriptPubKey: ByteVector,
+                               secret: ByteVector = ByteVector.empty) extends HostedChannelMessage {
+  require(secret.size <= 64, s"Hosted channel secret size=${secret.size}, max allowed=64")
+  val finalSecret: ByteVector = secret.take(64)
+}
+
+case class InitHostedChannel(maxHtlcValueInFlightMsat: UInt64,
+                             htlcMinimumMsat: MilliSatoshi,
+                             maxAcceptedHtlcs: Int,
+                             channelCapacityMsat: MilliSatoshi,
+                             liabilityDeadlineBlockdays: Int,
+                             minimalOnchainRefundAmountSatoshis: Satoshi,
+                             initialClientBalanceMsat: MilliSatoshi) extends HostedChannelMessage
+
+case class HostedChannelBranding(rgbColor: Color, pngIcon: ByteVector) extends HostedChannelMessage
+
+case class LastCrossSignedState(refundScriptPubKey: ByteVector,
+                                initHostedChannel: InitHostedChannel,
+                                blockDay: Long,
+                                localBalanceMsat: MilliSatoshi,
+                                remoteBalanceMsat: MilliSatoshi,
+                                localUpdates: Long,
+                                remoteUpdates: Long,
+                                incomingHtlcs: List[UpdateAddHtlc],
+                                outgoingHtlcs: List[UpdateAddHtlc],
+                                remoteSigOfLocal: ByteVector64,
+                                localSigOfRemote: ByteVector64) extends HostedChannelMessage {
+
+  lazy val reverse: LastCrossSignedState =
+    copy(localUpdates = remoteUpdates, remoteUpdates = localUpdates,
+      localBalanceMsat = remoteBalanceMsat, remoteBalanceMsat = localBalanceMsat,
+      remoteSigOfLocal = localSigOfRemote, localSigOfRemote = remoteSigOfLocal,
+      incomingHtlcs = outgoingHtlcs, outgoingHtlcs = incomingHtlcs)
+
+  lazy val hostedSigHash: ByteVector32 = {
+    val inPayments = incomingHtlcs.map(LightningMessageCodecs.updateAddHtlcCodec.encode(_).require.toByteVector).sortWith(LexicographicalOrdering.isLessThan)
+    val outPayments = outgoingHtlcs.map(LightningMessageCodecs.updateAddHtlcCodec.encode(_).require.toByteVector).sortWith(LexicographicalOrdering.isLessThan)
+
+    val preimage =
+      refundScriptPubKey ++
+        Protocol.writeUInt16(initHostedChannel.liabilityDeadlineBlockdays, ByteOrder.LITTLE_ENDIAN) ++
+        Protocol.writeUInt64(initHostedChannel.minimalOnchainRefundAmountSatoshis.toLong, ByteOrder.LITTLE_ENDIAN) ++
+        Protocol.writeUInt64(initHostedChannel.channelCapacityMsat.toLong, ByteOrder.LITTLE_ENDIAN) ++
+        Protocol.writeUInt64(initHostedChannel.initialClientBalanceMsat.toLong, ByteOrder.LITTLE_ENDIAN) ++
+        Protocol.writeUInt32(blockDay, ByteOrder.LITTLE_ENDIAN) ++
+        Protocol.writeUInt64(localBalanceMsat.toLong, ByteOrder.LITTLE_ENDIAN) ++
+        Protocol.writeUInt64(remoteBalanceMsat.toLong, ByteOrder.LITTLE_ENDIAN) ++
+        Protocol.writeUInt32(localUpdates, ByteOrder.LITTLE_ENDIAN) ++
+        Protocol.writeUInt32(remoteUpdates, ByteOrder.LITTLE_ENDIAN) ++
+        inPayments.foldLeft(ByteVector.empty) { case (acc, htlc) => acc ++ htlc } ++
+        outPayments.foldLeft(ByteVector.empty) { case (acc, htlc) => acc ++ htlc }
+
+    Crypto.sha256(preimage)
+  }
+
+  def verifyRemoteSig(pubKey: PublicKey): Boolean = Crypto.verifySignature(hostedSigHash, remoteSigOfLocal, pubKey)
+
+  def withLocalSigOfRemote(priv: PrivateKey): LastCrossSignedState = copy(localSigOfRemote = Crypto.sign(reverse.hostedSigHash, priv))
+
+  def isAhead(remoteLCSS: LastCrossSignedState): Boolean = remoteUpdates > remoteLCSS.localUpdates || localUpdates > remoteLCSS.remoteUpdates
+
+  def isEven(remoteLCSS: LastCrossSignedState): Boolean = remoteUpdates == remoteLCSS.localUpdates && localUpdates == remoteLCSS.remoteUpdates
+
+  def stateUpdate(isTerminal: Boolean): StateUpdate = StateUpdate(blockDay, localUpdates, remoteUpdates, localSigOfRemote, isTerminal)
+}
+
+case class StateUpdate(blockDay: Long, localUpdates: Long, remoteUpdates: Long, localSigOfRemoteLCSS: ByteVector64, isTerminal: Boolean) extends HostedChannelMessage
+
+case class StateOverride(blockDay: Long, localBalanceMsat: MilliSatoshi, localUpdates: Long, remoteUpdates: Long, localSigOfRemoteLCSS: ByteVector64) extends HostedChannelMessage
+
+// PHC
+
+trait HostedChannelRoutingMessage
+
+case class QueryPublicHostedChannels(chainHash: ByteVector32) extends HostedChannelMessage with HostedChannelRoutingMessage
+
+case class ReplyPublicHostedChannelsEnd(chainHash: ByteVector32) extends HostedChannelMessage with HostedChannelRoutingMessage
---
+++ src/main/scala/fr/acinq/hc/app/wire/Codecs.scala
@@ -0,0 +1,65 @@
+package fr.acinq.hc.app.wire
+
+import fr.acinq.hc.app._
+import fr.acinq.eclair.wire.CommonCodecs._
+import scodec.codecs.{bool, listOfN, uint16, uint32}
+import fr.acinq.eclair.wire.LightningMessageCodecs
+import scodec.Codec
+
+
+object Codecs {
+  val invokeHostedChannelCodec: Codec[InvokeHostedChannel] = {
+    (bytes32 withContext "chainHash") ::
+      (varsizebinarydata withContext "refundScriptPubKey") ::
+      (varsizebinarydata withContext "secret")
+  }.as[InvokeHostedChannel]
+
+  val initHostedChannelCodec: Codec[InitHostedChannel] = {
+    (uint64 withContext "maxHtlcValueInFlightMsat") ::
+      (millisatoshi withContext "htlcMinimumMsat") ::
+      (uint16 withContext "maxAcceptedHtlcs") ::
+      (millisatoshi withContext "channelCapacityMsat") ::
+      (uint16 withContext "liabilityDeadlineBlockdays") ::
+      (satoshi withContext "minimalOnchainRefundAmountSatoshis") ::
+      (millisatoshi withContext "initialClientBalanceMsat")
+  }.as[InitHostedChannel]
+
+  val hostedChannelBrandingCodec: Codec[HostedChannelBranding] = {
+    (rgb withContext "rgbColor") ::
+      (varsizebinarydata withContext "pngIcon")
+  }.as[HostedChannelBranding]
+
+  val lastCrossSignedStateCodec: Codec[LastCrossSignedState] = {
+    (varsizebinarydata withContext "refundScriptPubKey") ::
+      (initHostedChannelCodec withContext "initHostedChannel") ::
+      (uint32 withContext "blockDay") ::
+      (millisatoshi withContext "localBalanceMsat") ::
+      (millisatoshi withContext "remoteBalanceMsat") ::
+      (uint32 withContext "localUpdates") ::
+      (uint32 withContext "remoteUpdates") ::
+      (listOfN(uint16, LightningMessageCodecs.updateAddHtlcCodec) withContext "incomingHtlcs") ::
+      (listOfN(uint16, LightningMessageCodecs.updateAddHtlcCodec) withContext "outgoingHtlcs") ::
+      (bytes64 withContext "remoteSigOfLocal") ::
+      (bytes64 withContext "localSigOfRemote")
+  }.as[LastCrossSignedState]
+
+  val stateUpdateCodec: Codec[StateUpdate] = {
+    (uint32 withContext "blockDay") ::
+      (uint32 withContext "localUpdates") ::
+      (uint32 withContext "remoteUpdates") ::
+      (bytes64 withContext "localSigOfRemoteLCSS") ::
+      (bool withContext "isTerminal")
+  }.as[StateUpdate]
+
+  val stateOverrideCodec: Codec[StateOverride] = {
+    (uint32 withContext "blockDay") ::
+      (millisatoshi withContext "localBalanceMsat") ::
+      (uint32 withContext "localUpdates") ::
+      (uint32 withContext "remoteUpdates") ::
+      (bytes64 withContext "localSigOfRemoteLCSS")
+  }.as[StateOverride]
+
+  val queryPublicHostedChannelsCodec: Codec[QueryPublicHostedChannels] = (bytes32 withContext "chainHash").as[QueryPublicHostedChannels]
+
+  val replyPublicHostedChannelsEndCodec: Codec[ReplyPublicHostedChannelsEnd] = (bytes32 withContext "chainHash").as[ReplyPublicHostedChannelsEnd]
+}
